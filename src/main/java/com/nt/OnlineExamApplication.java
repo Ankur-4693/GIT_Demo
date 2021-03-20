f606704d2f5a21302cfe81be04ac6b7c61623b15
Hi <ankur.wadatkar@gmail.com>
2021-03-20 08:06:23 +0530

    comment added in branch 1

--- src/main/java/com/nt/OnlineExamApplication.java
+++ src/main/java/com/nt/OnlineExamApplication.java
@@ -2,7 +2,7 @@ package com.nt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// this is added to branch 1
 @SpringBootApplication(scanBasePackages = "com.nt.controller")
 public class OnlineExamApplication {
 
